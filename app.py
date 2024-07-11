8724277c3f448fdb30c3cc5c65f9fda5ab13e2c0
Ralph <91218756+RalphilouT@users.noreply.github.com>
2024-07-10 20:41:22 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,3 @@
-# from flask import Flask, render_template, request, jsonify
-
-# from chat import get_response
-
-# app = Flask(__name__)
-
-# @app.get("/")
-# def index_get():
-#     return render_template("base.html")
-
-
-# @app.post("/predict")
-# def predict():
-#     text = request.get_json().get("message")
-#     # TODO: check if text is valid
-
-#     resp = get_response(text)
-#     message = {"answer": resp}
-#     return jsonify(message)
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from chat import get_response
